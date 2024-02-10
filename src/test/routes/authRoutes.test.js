0e790ada563da46c76d51977d42b212a1fad1b78
leonardopadilha <leonardopadilha28@gmail.com>
2024-02-10 13:30:19 -0300

    Login com email e senha

--- src/test/routes/authRoutes.test.js
+++ src/test/routes/authRoutes.test.js
@@ -1,2 +1,29 @@
-import { describe, expect, it, jest } from '@jest/globals';
+import { beforeEach, afterEach, describe, expect, it, jest } from '@jest/globals';
+
 import request from 'supertest';
+
+import app from '../../app';
+
+let servidor;
+beforeEach(() => {
+    const porta = 3000;
+    servidor = app.listen(porta);
+});
+
+afterEach(() => {
+    servidor.close();
+});
+
+describe('Testando a roda login (POST)', () => {
+    it('O login deve possuir um email e senha para se autenticar', async () => {
+        const loginMock = {
+            email: 'teste@teste.com.br'
+        }
+
+        await request(servidor)
+                    .post('/login')
+                    .send(loginMock)
+                    .expect(500)
+                    .expect('"A senha de usuario é obrigatório."')
+    })
+})
